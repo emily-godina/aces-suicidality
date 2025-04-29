b7a1c6da0c432cd54b114ea5540189d8690f38ee
emily <egodina@uw.edu>
2025-04-29 14:04:35 -0700

    created new script for table 1

--- D_exploratory-analysis.R
+++ D_exploratory-analysis.R
@@ -1,4 +1,18 @@
 # TITLE: WA BRFSS 2020 - Exploratory Analysis 
 # Last Edited: 04-29-25
-# Description: In this script, we will conduct exploratory analysis and create
-    # a Table 1. 
+# Description: In this script, we will conduct exploratory analysis and 
+    # create a Table 1. 
+
+
+### ---- SETUP & DATA IMPORT ---- ###
+
+#clearing workspace
+rm(list = ls())
+
+#loading packages
+library(tidyverse)
+library(reactable)
+library(gt)
+
+#importing dataset
+brfss20 <- read_dta("brfss_2020.dta")
