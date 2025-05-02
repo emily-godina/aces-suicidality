13621739c757b0adc67a800ccb0c26cd5d9429c3
Charlotte <brynec@uw.edu>
2025-05-02 11:07:47 -0700

    new

---
+++ B_final-cleaning.R
@@ -0,0 +1,130 @@
+# TITLE: WA BRFSS 2020 - Data cleaning 2
+# Last Edited: 05-1-2025
+# Description: In this script, we will clean and recode our dataset.
+
+
+### ---- SETUP & DATA IMPORT ---- ###
+
+#clearing workspace
+rm(list = ls())
+
+#loading packages
+library(tidyverse)
+library(reactable)
+library(gt)
+
+#importing dataset
+brfss20 <- read_dta("brfss_2020.dta")
+View(brfss20)
+
+#remove all mental health k6 items
+brfss20 <- brfss20 %>%
+  select(-misnervs, -mishopls, -misrstls, -misdeprd, -miseffrt, -miswtles, 
+         -mistmnt, -mi_score, -smi)
+
+#remove unused ace variables
+brfss20 <- brfss20 %>%
+  select(-acedeprs2, -acedrink, -acedrugs, -aceprisn, -acedivrc,
+         -acetouch2, -acethem2, -acehvsx2)
+
+
+
+#remove _ from variables
+names(brfss20) <- sub("^_", "", names(brfss20))
+
+
+#categorize age into 6 bins
+brfss20 <- brfss20 %>%
+  mutate(age_group = case_when(
+    age >= 18 & age <= 24 ~ "18-24",
+    age >= 25 & age <= 34 ~ "25-34",
+    age >= 35 & age <= 44 ~ "35-44",
+    age >= 45 & age <= 54 ~ "45-54",
+    age >= 55 & age <= 64 ~ "55-64",
+    age >= 65             ~ "65 or older",
+    TRUE ~ NA_character_
+  )) %>%
+  mutate(age_group = factor(age_group, levels = c("18-24", "25-34", "35-44", 
+                                                  "45-54", "55-64", "65 or older")))
+
+
+#label sex
+brfss20 <- brfss20 %>%
+  mutate(sex_f = factor(sex, levels = c(1, 2), labels = c("Male", "Female")))
+
+
+
+#condense and label race/ethnicity
+brfss20 <- brfss20 %>%
+  mutate(
+    raceth_f = case_when(
+      hispanc == 1 ~ "Hispanic",
+      hispanc == 2 & prace1 == 1 ~ "White, NH",
+      hispanc == 2 & prace1 == 2 ~ "Black, NH",
+      hispanc == 2 & prace1 == 3 ~ "Asian, NH",
+      hispanc == 2 & prace1 == 4 ~ "AI/AN, NH",
+      hispanc == 2 & prace1 == 6 ~ "Other race, NH",
+      TRUE ~ NA_character_
+    ),
+    raceth_f = factor(raceth_f, levels = c(
+      "White, NH", "Black, NH", "Asian, NH", "AI/AN, NH", "Other race, NH", "Hispanic"))
+  )
+
+view(brfss20)
+
+
+#recode ace variables into 1,0, NAs
+brfss20 <- brfss20 %>%
+  mutate(
+    acepunch2 = ifelse(acepunch2 %in% c(7, 9), NA, ifelse(acepunch2 == 1, 1, 0)), #household violence
+    acehurt2  = ifelse(acehurt2 %in% c(7, 9), NA, ifelse(acehurt2 == 1, 1, 0)),   #physical abuse
+    aceswear2 = ifelse(aceswear2 %in% c(7, 9), NA, ifelse(aceswear2 == 1, 1, 0))  #verbal abuse
+  )
+
+
+#label factor ace variables
+brfss20 <- brfss20 %>%
+  mutate(
+    acepunch_f = factor(acepunch2, levels = c(0, 1), labels = c("No", "Yes")),   #household violence
+    acehurt_f  = factor(acehurt2,  levels = c(0, 1), labels = c("No", "Yes")),   #physical abuse
+    aceswear_f = factor(aceswear2, levels = c(0, 1), labels = c("No", "Yes"))    #verbal abuse
+  )
+
+
+#recode and label suicide
+brfss20 <- brfss20 %>%
+  mutate(
+    suicide = case_when(
+      suicide == 1 ~ 1,
+      suicide == 2 ~ 0,
+      suicide %in% c(7, 9) ~ NA_real_,
+      TRUE ~ NA_real_
+    ),
+    suicide_f = factor(suicide, levels = c(0, 1), labels = c("No", "Yes"))
+  )
+
+
+
+#create table 1
+# need to decide title/caption, what we're doing with NAs "unknowns"
+table1 <- brfss20 %>%
+  select(suicide_f, sex_f, age_group, raceth_f,
+         acepunch_f, acehurt_f, aceswear_f) %>%
+  tbl_summary(
+    by = suicide_f,
+    missing = "ifany",
+    percent = "column",
+    label = list(
+      sex_f ~ "Sex",
+      age_group ~ "Age (years)",
+      raceth_f ~ "Race/Ethnicity",
+      acepunch_f ~ "Witnessed Household Violence (Hit/Hurt)",
+      acehurt_f ~ "Physical Abuse (Hit/Hurt)",
+      aceswear_f ~ "Verbal Abuse (Sworn/Insulted)"
+    )
+  )
+
+#view table1
+table1
+
+nrow(brfss20)
